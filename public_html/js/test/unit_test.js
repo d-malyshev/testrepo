acc8b3ef79fcd6a685ffcf681f2dd0273868badb
Dmitry Malyshev <Domitry_Malyshev@voyagegroup.com>
2014-09-22 17:01:28 +0900

    QUinit.config.autostartを修正

--- public_html/js/test/unit_test.js
+++ public_html/js/test/unit_test.js
@@ -2,8 +2,8 @@
  *
  */
 
-// Test環境のみで実行
-QUnit.config.autostart = (window.location.href.indexOf("test.myserver.com") > -1) ? true : false;
+// 本番以外の環境で実行
+QUnit.config.autostart = (window.location.href.indexOf("prod.myserver.com") > -1) ? false : true;
 
 // Test環境のみで必要なdivやcssを追加
 if (QUnit.config.autostart != false) {
